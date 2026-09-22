69ee9c6789ac7db5de9ce85a5d10610a61d977f2
Josue Llontop <josuellontopvivas@gmail.com>
2026-09-22 16:08:07 -0500

    style(tema): configurar Material 3 con paleta de colores y modo oscuro

---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/andinasalud/presentation/theme/Color.kt
@@ -0,0 +1,16 @@
+package pe.edu.upeu.andinasalud.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Paleta principal sugerida para la clínica
+val PrimaryLight = Color(0xFF006C4C)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val SecondaryLight = Color(0xFF4C6357)
+val BackgroundLight = Color(0xFFFBFDF9)
+val SurfaceLight = Color(0xFFFBFDF9)
+
+val PrimaryDark = Color(0xFF89D6AF)
+val OnPrimaryDark = Color(0xFF003825)
+val SecondaryDark = Color(0xFFB3CCBE)
+val BackgroundDark = Color(0xFF191C1A)
+val SurfaceDark = Color(0xFF191C1A)
