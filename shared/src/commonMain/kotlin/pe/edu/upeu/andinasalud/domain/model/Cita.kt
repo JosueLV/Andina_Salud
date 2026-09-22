4e59ff1cedf0c6749a6f9e550fe8d0f3d8eb36f1
Josue Llontop <josuellontopvivas@gmail.com>
2026-09-22 15:34:28 -0500

    feat(dominio): crear modelos de datos

---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/andinasalud/domain/model/Cita.kt
@@ -0,0 +1,11 @@
+package pe.edu.upeu.andinasalud.domain.model
+
+data class Cita(
+    val id: Int,
+    val especialidad: String,
+    val medico: String,
+    val sede: String,
+    val fecha: String,
+    val hora: String,
+    val estado: EstadoCita
+)
---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/andinasalud/domain/model/Paciente.kt
@@ -0,0 +1,8 @@
+package pe.edu.upeu.andinasalud.domain.model
+
+data class Paciente(
+    val id: String,
+    val nombre: String,
+    val documento: String,
+    val correo: String
+)
